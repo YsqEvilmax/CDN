3f6fb6a651e7078a772cbb2f614f72be20c33c03
YsqEvilmax <ysq.evilmax@qq.com>
2015-10-02 17:23:55 +1300

    1.BinaryFormatter does not work, the length of message will change after sent. 2.XmlSerializer works well, but it can not serialize TreeNode because of menucontex. 2.SoapFormatter works for both sending and serializing.This version I use soap. However, it requires a special constructor

--- CDNCommon/CDNNetWork.cs
+++ CDNCommon/CDNNetWork.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
@@ -16,24 +18,29 @@ namespace CDN
     [Serializable]
     public class CDNMessage : ISerializable, ICloneable
     {
-        public enum MSGID : int { TEST, SHOW, DOWNLOAD};
+        public enum MSGID : int { TEST, SHOW, DOWNLOAD };
 
         public CDNMessage()
         {
         }
 
         public CDNMessage(EndPoint point)
-            :this()
+            : this()
         {
             String[] ip = point.ToString().Split(':');
             this.address = ip[0];
             this.port = int.Parse(ip[1]);
         }
 
-        public void Fill(MSGID id, String content)
+        #region Serialization Control
+        //This function is necessary for soap Serialization
+        protected CDNMessage(SerializationInfo info, StreamingContext context)
         {
-            this.id = id;
-            this.content = content;
+            if (info == null) { throw new System.ArgumentNullException("info"); }
+            this.id = (MSGID)info.GetUInt32("id");
+            this.content = info.GetString("content");
+            this.address = info.GetString("address");
+            this.port = info.GetInt32("port");
         }
 
         public void GetObjectData(SerializationInfo info, StreamingContext context)
@@ -43,6 +50,13 @@ namespace CDN
             info.AddValue("content", this.content);
             info.AddValue("address", this.address);
             info.AddValue("port", this.port);
+        }
+        #endregion
+
+        public void Fill(MSGID id, String content)
+        {
+            this.id = id;
+            this.content = content;
         }
 
         public object Clone()
@@ -66,23 +80,37 @@ namespace CDN
     {
         static public String Serialize(T t)
         {
-            Type x = t.GetType();
-            XmlSerializer ser = new XmlSerializer(t.GetType());
-            StringBuilder sb = new StringBuilder();
-            StringWriter writer = new StringWriter(sb);
-            ser.Serialize(writer, t);
-            return sb.ToString();
+            String result = "";
+            try
+            {
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    IFormatter ser = new SoapFormatter();
+                    ser.Serialize(stream, t);
+                    result = Encoding.UTF8.GetString(stream.GetBuffer());
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return result;
         }
 
         static public T Deserialize(String s)
         {
-            T t = new T();       
-            XmlDocument xdoc = new XmlDocument();
-            xdoc.LoadXml(s);
-            XmlNodeReader reader = new XmlNodeReader(xdoc.DocumentElement);
-            XmlSerializer ser = new XmlSerializer(t.GetType());
-            object obj = ser.Deserialize(reader);
-            return obj as T;
+            T t = null;
+            try
+            {
+                using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(s)))
+                {
+                    IFormatter ser = new SoapFormatter();
+                    t = ser.Deserialize(stream) as T;
+                }
+            }
+            catch (Exception e)
+            { Console.WriteLine(e); }
+            return t;
         }
     }
 
@@ -102,7 +130,7 @@ namespace CDN
 
         public async Task Idle()
         {
-            while(true)
+            while (true)
             {
                 TcpClient client = await AcceptTcpClientAsync();
                 NetworkStream ns = client.GetStream();
@@ -122,8 +150,8 @@ namespace CDN
 
         public override string ToString()
         {
-            return "IP:\r\n" + base.LocalEndpoint.ToString() + "\r\n" 
-                + "FileSystemDepository:\r\n"+ fsd.ToString() + "\r\n";
+            return "IP:\r\n" + base.LocalEndpoint.ToString() + "\r\n"
+                + "FileSystemDepository:\r\n" + fsd.ToString() + "\r\n";
         }
         public FileSystemDepository fsd { get; protected set; }
     }
--- CDNCommon/FileDepository.cs
+++ CDNCommon/FileDepository.cs
@@ -38,12 +38,27 @@ namespace CDN
 
     [Serializable]
     public abstract class CommonNode : TreeNode
-    {       
+    {
+        public CommonNode()
+            : base()
+        { }
+
+        protected CommonNode(SerializationInfo si, StreamingContext context)
+            : base(si, context)
+        {
+        }
+
+        protected override void Serialize(SerializationInfo si, StreamingContext context)
+        {
+            base.Serialize(si, context);
+            //si.AddValue("FileSystem", this.info);
+        }
+
         public override string ToString()
         {
             String prefix = new String(' ', Level);
-            String result = prefix + info.Name + "\r\n";           
-            foreach(TreeNode tn in Nodes)
+            String result = prefix + info.Name + "\r\n";
+            foreach (TreeNode tn in Nodes)
             {
                 result += (tn as CommonNode).ToString();
             }
@@ -53,6 +68,7 @@ namespace CDN
         protected FileSystemInfo info;
     }
 
+    [Serializable]
     public class DirectoryNode : CommonNode
     {
         public DirectoryNode()
@@ -65,18 +81,30 @@ namespace CDN
             info = new DirectoryInfo(path);
         }
 
+        protected DirectoryNode(SerializationInfo si, StreamingContext context)
+            : base(si, context)
+        {
+            this.info = new DirectoryInfo(si.GetString("DirectoryInfo"));
+        }
+
+        protected override void Serialize(SerializationInfo si, StreamingContext context)
+        {
+            base.Serialize(si, context);
+            si.AddValue("DirectoryInfo", this.info);
+        }
+
         public void Scan()
         {
             //Get all the files in this directory, not these directories
             FileInfo[] fileList = (info as DirectoryInfo).GetFiles();
-            foreach(FileInfo fi in fileList)
+            foreach (FileInfo fi in fileList)
             {
                 FileNode subNode = new FileNode(fi.FullName);
                 this.Nodes.Add(subNode);
             }
             //Get all the directories in this directory, not these files
             DirectoryInfo[] dirList = (info as DirectoryInfo).GetDirectories();
-            foreach(DirectoryInfo di in dirList)
+            foreach (DirectoryInfo di in dirList)
             {
                 DirectoryNode subNode = new DirectoryNode(di.FullName);
                 this.Nodes.Add(subNode);
@@ -85,16 +113,30 @@ namespace CDN
         }
     }
 
+    [Serializable]
     public class FileNode : CommonNode
     {
         public FileNode()
-            :base()
-        { }
+            : base()
+        {
+        }
 
         public FileNode(String path)
             : this()
         {
             info = new FileInfo(path);
+        }
+
+        protected FileNode(SerializationInfo si, StreamingContext context)
+            : base(si, context)
+        {
+            this.info = new FileInfo(si.GetString("FileInfo"));
+        }
+
+        protected override void Serialize(SerializationInfo si, StreamingContext context)
+        {
+            base.Serialize(si, context);
+            si.AddValue("FileInfo", this.info);
         }
     }
 }
